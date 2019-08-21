85fb05b6fe5d267aec1693145e951279fa9b26db
zippppy <45407709+zippppy@users.noreply.github.com>
2019-08-21 01:21:00 -0400

    Add files via upload

---
+++ UnitTestHN/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HNTest;
+
+
+namespace UnitTestHN
+{
+    [TestClass]
+    public class UnitTest1
+    {
+ 
+        [TestMethod]
+        public async Task HNLTest()
+        {
+            HN hn = new HN();
+
+            var x = await hn.Newest();
+            Assert.IsTrue(x.Count > 0);
+
+        }
+    }
+}
